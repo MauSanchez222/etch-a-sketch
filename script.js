35de8bebee2210308a2479009b62a8804e52551c
Mauri <mauricioasan96@gmail.com>
2022-08-24 15:39:20 -0300

    changed the way tiles are colored and added random rgb coloring for sicko mode

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 const sketchZone = document.getElementById('tiles-zone');
-const normButt = document.getElementById('normal');
+const normBtn = document.getElementById('normal');
+const sickBtn = document.getElementById('sicko');
 const eraser = document.getElementById('reset');
+const sketchTiles = document.getElementsByClassName('tile-white');
+let mode = 'normal';
 
 function loadTiles(){
     for (let i=255; i>0; i--){
         const sketchTile = document.createElement('div');
         sketchTile.classList.add('tile-white');
-        sketchTile.addEventListener('mouseenter', function(){
-            sketchTile.classList.toggle('tile-black')
-        });
+        sketchTile.addEventListener('mouseenter', styleTiles)
         sketchZone.appendChild(sketchTile);     
+    }
+}
+
+function eraseTiles(){
+    const tiles = document.getElementsByClassName('tile-white');
+    for (let i = 0; i < tiles.length; i++){
+        tiles[i].style.backgroundColor = 'white';
+    }
+}
+
+function styleTiles(e){
+    if (mode === 'normal'){
+        e.target.style.backgroundColor = 'black';
+    }
+    else{
+        var randomR = Math.floor(Math.random() * 256);
+        var randomG = Math.floor(Math.random() * 256);
+        var randomB = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${randomR},${randomG},${randomB})`
     }
 }
 
 document.addEventListener('DOMContentLoaded', function(){
     loadTiles();
+    styleTiles();
 })
 
-eraser.addEventListener('click', function(e){
-    const tiles = document.getElementsByClassName('tile-black');
-    Array.from(tiles).forEach(function(tile){
-        tile.classList.remove('tile-black');
-    })
+
+sickBtn.addEventListener('click', function(){
+    mode = 'sicko';
 });
+
+normBtn.addEventListener('click', function(){
+    mode = 'normal';
+})
+
+
+eraser.addEventListener('click', () => eraseTiles());
+
+
