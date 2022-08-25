c172a04d9496cccd2951fc836115d2bcf7975bd4
Mauri <mauricioasan96@gmail.com>
2022-08-24 21:38:38 -0300

    added resizing of sketch board aka changing ammount of rows and columns

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const normBtn = document.getElementById('normal');
 const sickBtn = document.getElementById('sicko');
 const eraser = document.getElementById('reset');
 const sketchTiles = document.getElementsByClassName('tile-white');
+const resizeBtn = document.getElementById('resize');
+const resizeInput = document.getElementById('resize-number')
 let mode = 'normal';
+let size = 255;
 
-function loadTiles(){
-    for (let i=255; i>0; i--){
+function loadTiles(size){
+
+    for (let i=size; i>0; i--){
         const sketchTile = document.createElement('div');
         sketchTile.classList.add('tile-white');
         sketchTile.addEventListener('mouseenter', styleTiles)
@@ -18,6 +22,16 @@ function eraseTiles(){
     const tiles = document.getElementsByClassName('tile-white');
     for (let i = 0; i < tiles.length; i++){
         tiles[i].style.backgroundColor = 'white';
+    }
+}
+
+function setTileSize(size){
+    let tileHeightWidth = (480/size)-2;
+    console.log(tileHeightWidth);
+    const tiles = document.getElementsByClassName('tile-white');
+    for (let i = 0; i < tiles.length; i++){
+        tiles[i].style.height = `${tileHeightWidth}px`;
+        tiles[i].style.width = `${tileHeightWidth}px`;
     }
 }
 
@@ -33,8 +47,16 @@ function styleTiles(e){
     }
 }
 
+function obtainValue(){
+    size = document.getElementById('resize-number').value;
+    if (size === ""){
+        size = 16;
+    }
+    return size ;
+}
+
 document.addEventListener('DOMContentLoaded', function(){
-    loadTiles();
+    loadTiles(size);
     styleTiles();
 })
 
@@ -47,6 +69,13 @@ normBtn.addEventListener('click', function(){
     mode = 'normal';
 })
 
+resizeBtn.addEventListener('click', function(){
+    let tileHW = obtainValue();
+    sketchZone.innerHTML = '';
+    loadTiles(tileHW**2);
+    setTileSize(tileHW);
+    resizeInput.value = "";
+});
 
 eraser.addEventListener('click', () => eraseTiles());
 
